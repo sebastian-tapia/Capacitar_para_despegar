382843abdfce39d8bdfe568e6fcd11713db9c7f6
Sebastian <sebastian.tapia35202@gmail.com>
2022-10-06 13:07:53 -0300

    primera clase objetos

---
+++ tareaEnClase.py
@@ -0,0 +1,23 @@
+class Maria:
+    def __init__(self,u_energia):
+        self.energia=u_energia
+    energia=40
+    def esta_debil(self):
+        return self.energia>10
+    def esta_feliz(self):
+        return self.energia>500
+
+class Chimuelo:
+    def __init__(self,energia,dientes):
+        self.energia=energia
+        self.dientes=dientes
+    def esta_debil(self):
+        return self.energia>50
+
+    def esta_feliz(self)
+        return self.energia>500
+ 
+class Hipo:
+    __innit__(self,)
+
+
